5544c521b5cf4fcccf73800489e9376bfea52868
M Surya <chesuryamurali@gmail.com>
2025-07-12 17:03:10 +0530

    Delete day-2backend directory

--- day-2backend/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "myprojectbucketforbackend"
-    key    = "project1/terraform.tfstate"
-    region = "ap-south-2"
-  }
-}
--- day-2backend/main.tf
+++
@@ -1,9 +0,0 @@
-    resource "aws_instance" "my-ec2" {
-    ami="ami-06a3dc84cf2fde367"
-    instance_type = "t3.micro"
-    count=1
-    key_name = "murali"
-    tags={
-      Name=  "s3-ec2"
-    }   
-  }
