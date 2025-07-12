43cd4800e1770a4d75e763e0ecacd1657ffba4ba
M Surya <chesuryamurali@gmail.com>
2025-07-12 17:02:51 +0530

    Delete day-1 directory

--- day-1/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myec2" {
-  ami           = var.ami
-  key_name      = var.key_name
-  region        = "ap-south-2"
-  instance_type = var.type
-  count         = var.countsurya
-  tags = {
-    Name = var.Name  }
-}
--- day-1/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-ami = "ami-06a3dc84cf2fde367"
-
-type = "t3.micro"
-key_name = "murali"
