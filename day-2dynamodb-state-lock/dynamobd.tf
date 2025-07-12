39f8e63e7ea37490e5b5f4778f14543ae6bddb1e
M Surya <chesuryamurali@gmail.com>
2025-07-12 17:05:27 +0530

    Delete day-2dynamodb-state-lock directory

--- day-2dynamodb-state-lock/dynamobd.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "myprojectbucketforbackend"
-    key    = "project2/terraform.tfstate"
-    region = "ap-south-2"
-    dynamodb_table = "mydynamodbtable"
-    encrypt = true
-  }
-}
